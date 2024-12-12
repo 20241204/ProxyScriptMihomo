5dfeb30ae647bbcaae49e0661621a015460c9d9a
UiLgNoDkOoLtUo <UiLgNoD-kOoLtUo@outlook.com>
2024-12-12 13:07:36 +0800

    2024-12-12 13:07:36 代码提交

---
+++ mihomo-start-amd64-linux.sh
@@ -0,0 +1,208 @@
+#!/usr/bin/env bash
+clear
+
+# 当前路径
+PWD=$(pwd)/sources
+mkdir -pv "${PWD}"
+cd "${PWD}"
+
+echo "开始吧小老弟！"
+echo "当前目录${PWD}"
+
+yamledit(){
+    # 删除替换修改 port: 7891 socks-port: 7892 nmixed-port: 7893 external-controller: :7894 redir-port: 7895 tproxy-port: 7896 allow-lan: true mode: rule log-level: debug secret: 
+    local config_file=$1
+
+    echo "正在编辑 ${config_file}，请稍候..."
+
+    declare -a delete_params=(
+    "port: "
+    "mixed-port: "
+    "socks-port: "
+    "allow-lan: "
+    "external-controller: "
+    "mode: "
+    "log-level: "
+    "redir-port: "
+    "tproxy-port: "
+    "secret: "
+    "external-ui: "
+    "external-ui-name: "
+    "external-ui-url: "
+    "tun:"
+    "[[:space:]]*enable: "
+    "[[:space:]]*device: "
+    "[[:space:]]*stack: "
+    "[[:space:]]*auto-route: "
+    "[[:space:]]*auto-redirect: "
+    "[[:space:]]*auto-detect-interface: "
+    "[[:space:]]*dns-hijack:"
+    "[[:space:]]*- any:"
+    "[[:space:]]*route-exclude-address: "
+    "[[:space:]]*mtu: "
+    )
+
+    declare -a add_params=(
+    "port: 7891"
+    "socks-port: 7892"
+    "mixed-port: 7893"
+    "external-controller: :7894"
+    "external-ui: ui"
+    "external-ui-name: xd"
+    "external-ui-url: https://github.com/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip"
+    "redir-port: 7895"
+    "tproxy-port: 7896"
+    "allow-lan: true"
+    "mode: rule"
+    "log-level: debug"
+    "tun:\n  enable: false\n  device: Mihomo\n  stack: mixed\n  auto-route: true\n  auto-redirect: false\n  auto-detect-interface: true\n  dns-hijack:\n    - any:53\n  route-exclude-address: []\n  mtu: 1500"
+    )
+
+    for param in "${delete_params[@]}"; do
+        echo "删除 ${config_file} 中开头为 \"${param}\" 的行："
+        sed -i "/^${param}/d" "${config_file}"
+        echo sed -i "/^${param}/d" "${config_file}"
+    done
+
+    for ((i=0; i<${#add_params[@]}; i++)); do
+        echo "在第$((i+1))行开头添加 ${add_params[i]}"
+        sed -i "$((i+1))i${add_params[i]}" "${config_file}"
+        echo sed -i "$((i+1))i${add_params[i]}" "${config_file}"
+    done
+}
+
+startclash(){
+echo "等待软件启动，请稍候..."
+echo "可能需要输入密码来干掉工具、edge和chrome(chromium)浏览器"
+sudo kill -9 `ps -ef | grep -v grep | grep mihomo-linux-amd64 | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep mihomo-linux-amd64 | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep msedge | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep microsoft-edge | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep microsoft-edge-dev | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep google-chrome | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep google-chrome-stable | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep chromium | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep chromium-browser | awk '{print $2}'`
+kill -9 `ps -ef | grep -v grep | grep tail | awk '{print $2}'`
+rm -rfv "*.log" chrome-user-data/SingletonLock edge-user-data/SingletonLock
+chmod u+x mihomo-linux-amd64
+nohup sudo  ${PWD}/mihomo-linux-amd64 -d  ${PWD}/.config/mihomo --ext-ui ${PWD}/.config/mihomo/ui -f ${PWD}/.config/mihomo/config.yaml > mihomo-linux-amd64.log 2>&1 & disown
+
+# 判断浏览器进程位置是否存在
+if [ -e /usr/bin/microsoft-edge ];then
+echo "microsoft-edge-dev true"
+# microsoft-edge
+nohup /usr/bin/microsoft-edge --no-sandbox --disable-gpu --disable-software-rasterizer --user-data-dir="${PWD}/chrome-user-data" --proxy-server=http://127.0.0.1:7891 "https://www.youtube.com/results?search_query=免费节点&sp=EgQIAxAB" > edge.log 2>&1 & disown
+elif [ -e /usr/bin/microsoft-edge-dev ];then
+echo "microsoft-edge-dev true"
+# microsoft-edge-dev
+nohup /usr/bin/microsoft-edge-dev --no-sandbox --disable-gpu --disable-software-rasterizer --user-data-dir="${PWD}/chrome-user-data" --proxy-server=http://127.0.0.1:7891 "https://www.youtube.com/results?search_query=免费节点&sp=EgQIAxAB" > edge.log 2>&1 & disown
+elif [ -e /usr/bin/google-chrome ];then
+echo "google-chrome true"
+# google-chrome
+nohup /usr/bin/google-chrome --no-sandbox --disable-gpu --disable-software-rasterizer --user-data-dir="${PWD}/chrome-user-data" --proxy-server=http://127.0.0.1:7891 "https://www.youtube.com/results?search_query=免费节点&sp=EgQIAxAB" > chrome.log 2>&1 & disown
+elif [ -e /usr/bin/google-chrome-stable ];then
+echo "google-chrome-stable true"
+# google-chrome-stable
+nohup /usr/bin/google-chrome-stable --no-sandbox --disable-gpu --disable-software-rasterizer --user-data-dir="${PWD}/chrome-user-data" --proxy-server=http://127.0.0.1:7891 "https://www.youtube.com/results?search_query=免费节点&sp=EgQIAxAB" > chrome.log 2>&1 & disown
+elif [ -e /usr/bin/chromium ];then
+echo "true"
+nohup /usr/bin/chromium --no-sandbox --disable-gpu --disable-software-rasterizer --user-data-dir="${PWD}/chrome-user-data" --proxy-server=http://127.0.0.1:7891 "https://www.youtube.com/results?search_query=免费节点&sp=EgQIAxAB" > chromium.log 2>&1 & disown
+elif [ -e /usr/lib64/chromium-browser/chromium-browser ];then
+echo "true"
+nohup /usr/lib64/chromium-browser/chromium-browser --no-sandbox --disable-gpu --disable-software-rasterizer --user-data-dir="${PWD}/chrome-user-data" --proxy-server=http://127.0.0.1:7891 "https://www.youtube.com/results?search_query=免费节点&sp=EgQIAxAB" > chromium.log 2>&1 & disown
+elif [ -e /usr/lib/chromium-browser/chromium-browser ];then
+echo "true"
+nohup /usr/lib/chromium-browser/chromium-browser --no-sandbox --disable-gpu --disable-software-rasterizer --user-data-dir="${PWD}/chrome-user-data" --proxy-server=http://127.0.0.1:7891 "https://www.youtube.com/results?search_query=免费节点&sp=EgQIAxAB" > chromium.log 2>&1 & disown
+else
+echo -e "chromium/edge/google-chrome浏览器不存在或没有正确安装，请尝试重新安装chromium/google-chrome浏览器\n或者您可以通过安装snap来安装chromium/google-chrome，这样会比较好"
+fi
+
+tail -200f mihomo-linux-amd64.log
+}
+
+choices(){
+echo 说明
+echo 一、此脚本支持chromium/google-chrome/edge浏览器，所以需要安装chromium/google-chrome浏览器，如果有兴趣可以自己DIY别的浏览器。
+echo "输入 1..12 通过联网更新文件使用，输入 13 随机执行现有文件:"
+read choice 
+if [ $choice -eq 1 ];then
+# ./wget --verbose --show-progress=on --progress=bar --hsts-file=/tmp/wget-hsts -c "http://gitlab.com/free9999/ipupdate/-/raw/master/clash/config.yaml"-O ".config/clash/config.yaml"
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经1启动程序！
+startclash
+elif [ $choice -eq 2 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/13/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经2启动程序！
+startclash
+elif [ $choice -eq 3 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/15/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经3启动程序！
+startclash
+elif [ $choice -eq 4 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/1/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经4启动程序！
+startclash
+elif [ $choice -eq 5 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/2/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经5启动程序！
+startclash
+elif [ $choice -eq 6 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/3/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经6启动程序！
+startclash
+elif [ $choice -eq 7 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/13/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经7启动程序！
+startclash
+elif [ $choice -eq 8 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/15/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经8启动程序！
+startclash
+elif [ $choice -eq 9 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经9启动程序！
+startclash
+elif [ $choice -eq 10 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/1/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经10启动程序！
+startclash
+elif [ $choice -eq 11 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/2/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经11启动程序！
+startclash
+elif [ $choice -eq 12 ];then
+curl -SL --connect-timeout 30 -m 60 --speed-time 30 --speed-limit 1 --retry 2 -H "Connection: keep-alive" -k "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/3/config.yaml" -o ".config/mihomo/config.yaml" -O
+yamledit ".config/mihomo/config.yaml"
+echo 已经12启动程序！
+startclash
+elif [ $choice -eq 13 ];then
+pool=(1 2 3 4 5 6 7 8 9 10 11 12)
+num=${#pool[*]}
+pn=${pool[$((RANDOM%num+1))]}
+echo $pn
+if [ "$pn" = "" ]; then
+	cp -r -v config1.yaml ".config/mihomo/config.yaml"
+else
+	cp -r -v config$pn.yaml ".config/mihomo/config.yaml"
+fi
+echo 已经随机启动程序！
+startclash
+else
+echo "what's up?"
+choices
+fi
+}
+
+choices
---
+++ mihomo-update-amd64-windows.ps1
@@ -0,0 +1,74 @@
+﻿# 清屏
+Clear-Host
+
+# 设置控制台颜色
+$Host.UI.RawUI.BackgroundColor = "Black"
+$Host.UI.RawUI.ForegroundColor = "Green"
+Clear-Host
+
+# 设置工作目录
+$PWD = "$PSScriptRoot\sources"
+Set-Location -Path $PWD
+
+Write-Host "当前目录: $PWD"
+
+# 定义 URL 列表和配置文件名列表
+$URLS = @(
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/13/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/15/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/1/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/2/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/3/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/13/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash/15/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/1/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/2/config.yaml",
+    "https://fastly.jsdelivr.net/gh/Alvin9999/pac2@latest/clash.meta2/3/config.yaml"
+)
+
+$CONFIG_FILES = @(
+    "config1.yaml",
+    "config2.yaml",
+    "config3.yaml",
+    "config4.yaml",
+    "config5.yaml",
+    "config6.yaml",
+    "config7.yaml",
+    "config8.yaml",
+    "config9.yaml",
+    "config10.yaml",
+    "config11.yaml",
+    "config12.yaml"
+)
+
+function Edit-Yaml {
+    param (
+        [string]$FilePath
+    )
+
+    Write-Host "正在编辑 $FilePath，请稍候..."
+    Write-Host "删除替换修改 port: 7891 socks-port: 7892 mixed-port: 7893 external-controller: :7894 redir-port: 7895 tproxy-port: 7896 allow-lan: true mode: rule log-level: debug secret: ?"
+    $content = Get-Content $FilePath | Where-Object { 
+        $_ -notmatch "^port: 7890|^mixed-port: 7890|^socks-port: 7891|^allow-lan: |^external-controller: |^mode: |^log-level: |^secret: " 
+    }
+    $newContent = @("port: 7891", "socks-port: 7892", "mixed-port: 7893", "external-controller: :7894", "external-ui: ui", "external-ui-name: xd", "external-ui-url: https://github.com/MetaCubeX/metacubexd/archive/refs/heads/gh-pages.zip",
+    "redir-port: 7895", "tproxy-port: 7896", "allow-lan: true", "mode: rule", "log-level: debug",
+    "tun:", "  enable: false", "  device: Mihomo", "  stack: mixed", "  auto-route: true", "  auto-redirect: false", "  auto-detect-interface: true", "  dns-hijack:", "    - any:53", "  route-exclude-address: []", "  mtu: 1500") + $content
+    $newContent | Set-Content $FilePath
+    Write-Host "替换 utf-8 crlf 为 lf 即 去除每行结尾的 `r"
+    (Get-Content $FilePath) -join "`n" | Set-Content -NoNewline $FilePath
+}
+
+# 更新配置文件并进行操作
+for ($i = 0; $i -lt $URLS.Length; $i++) {
+    Write-Host "Updating $($URLS[$i])"
+    Invoke-WebRequest -Uri $URLS[$i] -OutFile $CONFIG_FILES[$i] -Verbose
+
+    # 编辑 YAML 文件
+    Edit-Yaml -FilePath $CONFIG_FILES[$i]
+}
+cd $PSScriptRoot
+Write-Host "已经完成，请按回车键或空格键关闭此窗口！"
+Read-Host
